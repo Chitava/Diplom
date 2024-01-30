b15aa7d03daa0120238678578a02f9560f2d0ace
Читава Александр Валикоевич <chitava@lsin.ln>
2024-01-30 09:11:13 +0300

    Начало разработки дипломного проекта

---
+++ src/test/java/chitava/diplom/InbulkArApplicationTests.java
@@ -0,0 +1,13 @@
+package chitava.diplom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InbulkArApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
